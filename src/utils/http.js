947535bde911ed5830c93f68e624663247674117
LI <2943753234@qq.com>
2023-02-24 23:44:37 +0800

    chore: 修改baseUrl

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,62 +3,62 @@
 import axios from 'axios';
 
 // axios.defaults.timeout = 5000;
-axios.defaults.baseURL = 'http://101.132.132.215:3000';
+axios.defaults.baseURL = 'http://172.18.53.203:3000';
 // axios.defaults.baseURL = 'http://localhost:3000';
 
 
 //封装get方法
 export function get(url, params = {}) {
-    return new Promise((resolve, reject) => {
-        axios.get(url, {
-            params: params
-        })
-            .then(response => {
-                resolve(response);
-            })
-            .catch(err => {
-                reject(err);
-            })
+  return new Promise((resolve, reject) => {
+    axios.get(url, {
+      params: params
     })
+      .then(response => {
+        resolve(response);
+      })
+      .catch(err => {
+        reject(err);
+      })
+  })
 }
 
 //封装post请求
-export function post(url, data = {},config={}) {
-    return new Promise((resolve, reject) => {
-        axios.post(url, data,config)
-            .then(response => {
-                resolve(response);
-            })
-            .catch(err => {
-                reject(err);
-            })
-    })
+export function post(url, data = {}, config = {}) {
+  return new Promise((resolve, reject) => {
+    axios.post(url, data, config)
+      .then(response => {
+        resolve(response);
+      })
+      .catch(err => {
+        reject(err);
+      })
+  })
 }
 
 //封装put请求
 export function put(url, data = {}) {
-    return new Promise((resolve, reject) => {
-        axios.put(url, data)
-            .then(response => {
-                resolve(response);
-            })
-            .catch(err => {
-                reject(err);
-            })
-    })
+  return new Promise((resolve, reject) => {
+    axios.put(url, data)
+      .then(response => {
+        resolve(response);
+      })
+      .catch(err => {
+        reject(err);
+      })
+  })
 }
 
 //封装delete请求
-export function deleteDate(url){
-    return new Promise((resolve, reject) => {
-        axios.delete(url)
-            .then(response => {
-                resolve(response);
-            })
-            .catch(err => {
-                reject(err);
-            })
-    })
+export function deleteDate(url) {
+  return new Promise((resolve, reject) => {
+    axios.delete(url)
+      .then(response => {
+        resolve(response);
+      })
+      .catch(err => {
+        reject(err);
+      })
+  })
 }
 
 // //请求成功提示消息
